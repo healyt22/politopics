3846a23d87bbc4d050567789e8d9d263a7a0d598
Tim Healy <timothy.healy@oath.com>
2019-07-19 21:18:29 -0400

    changed data structures significant tweet cleaning

--- get_tweets.py
+++ get_tweets.py
@@ -6,7 +6,7 @@ log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s',
                 datefmt='%m/%d/%Y %I:%M:%S %p',
                 level=log.DEBUG)
 
-class ParseTweets():
+class GetTweets():
     def __init__(self):
         '''
         Initialize object with a Twitter API key file -> keys.yaml
@@ -22,15 +22,19 @@ class ParseTweets():
                 access_token_key=keys['access_token_key'],
                 access_token_secret=keys['access_token_secret'])
 
-    def get_politicians(self):
+    def get_politicians(self, slug):
         '''
         This method will return a list of politicians
         returns list object
         '''
-        members = self.api.GetListMembers(
-                slug='members-of-congress',
+        crooks = self.api.GetListMembers(
+                slug=slug,
                 owner_screen_name='cspan')
-        return(members)
+        for crook in crooks:
+            filepath = self.base_path + '/data/{0}'.format(crook.screen_name)
+            if not os.path.exists(filepath):
+                os.makedirs(filepath)
+        return(crooks)
 
     def get_tweets(self, handle, n):
         '''
@@ -38,29 +42,34 @@ class ParseTweets():
         TODO: figure out if date range should be implemented
         returns list object
         '''
-        timeline = self.api.GetUserTimeline(screen_name=handle, count = 1000)
-        tweets = [tweet.AsDict() for tweet in timeline]
-        filename = self.base_path + '/data/{0}_tweets.json'.format(handle)
-        with open(filename, 'w') as outfile:
-            json.dump(tweets, outfile, indent=4)
-        return(tweets)
+        timeline = self.api.GetUserTimeline(screen_name=handle, count = n)
+        for raw_tweet in timeline:
+            tweet = raw_tweet.AsDict()
+            params = {
+                'screen_name': tweet['user']['screen_name'],
+                'id': tweet['id']
+            }
+            filename = self.base_path + '/data/{screen_name}/{id}.json'.format(**params)
+            with open(filename, 'w') as outfile:
+                json.dump(tweet, outfile, indent=4)
 
-    def main(self, handle=''):
+    def main(self, handle='', n=200):
         '''
         Executes the program
         TODO: Explore efficiency of using loop vs. generator object
         '''
-        x = ParseTweets()
+        x = GetTweets()
         if handle is not None:
-            x.get_tweets(handle, 50)
+            x.get_tweets(handle, n)
         else:
-            crooks = x.get_politicians()
+            crooks = x.get_politicians('members-of-congress') #us-congress
             for crook in crooks:
-                x.get_tweets(crook.screen_name, 50)
+                log.info('Pulling tweets for: ' + crook.name)
+                x.get_tweets(crook.screen_name, n)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("-t", "--twitter_handle",
         help="Twitter handle to pull tweets against.")
     args = parser.parse_args()
-    ParseTweets().main(args.twitter_handle)
+    GetTweets().main(args.twitter_handle)
--- transform.py
+++ transform.py
@@ -1,6 +1,8 @@
-import os, json
+import os, json, string, re
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 from argparse import ArgumentParser
 import logging as log
 log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s',
@@ -8,43 +10,67 @@ log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s',
                 level=log.DEBUG)
 
 class Transform():
-    def __init__(self):
+    def __init__(self, handle):
         '''
         Initialize with raw json twitter data from /data directory
         '''
-        self.base_path = os.path.dirname(os.path.realpath(__file__))
-        self.data_dir = os.path.join(self.base_path,'data')
-        self.tweets = os.listdir(self.data_dir)
+        base_path = os.path.dirname(os.path.realpath(__file__))
+        data_dir = os.path.join(base_path,'data')
+        tweet_file = '/{0}_tweets.json'.format(handle)
+        filename = data_dir + tweet_file
+        with open(filename) as json_file:
+            self.data = json.load(json_file)
+        self.tweets = [tweet['text'] for tweet in self.data]
+        self.clean_tweets = []
+        self.main()
 
-    def preprocess_(self, text):
+    def preprocess(self, tweet):
         '''
         Some preprocessing will most likely need to be done to
         remove stop words, stemming, perhaps even by using synonyms
         TODO: NLTK library
         '''
+        tweet = re.sub(r"https\S+", "", tweet)
+        tokens = word_tokenize(tweet)
+        # convert to lower case
+        tokens = [w.lower() for w in tokens]
+        # remove punctuation from each word
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove remaining tokens that are not alphabetic
+        words = [word for word in stripped if word.isalpha()]
+        # filter out stop words
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
+        if words[0] == 'rt':
+            words = words[2:]
+        clean_words = ' '.join(words)
+        return(clean_words)
 
-    def construct_td_matrix(self, handle):
+    def construct_td_matrix(self):
         '''
         Construct a TD Matrix from inputted tweets data
         TODO: Implement term frequency - inverse document frequency
               (tf-idf) statistic
         '''
-        filename = self.base_path + '/data/{0}_tweets.json'.format(handle)
-        with open(filename) as json_file:
-            data = json.load(json_file)
-        tweet_text = [tweet['text'] for tweet in data]
         vec = CountVectorizer()
-        X = vec.fit_transform(tweet_text)
-        df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
-        print(df)
+        X = vec.fit_transform(self.clean_tweets)
+        self.td_matrix = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+        return(self.td_matrix)
 
-    def main(self, handle):
-        x = Transform()
-        x.construct_td_matrix(handle)
+    def main(self):
+        for i, tweet in enumerate(self.tweets):
+            try:
+                clean_tweet = self.preprocess(tweet)
+                self.clean_tweets.append(clean_tweet)
+            except IndexError:
+                log.warn('ERROR: ' + tweet)
+                continue
+        self.construct_td_matrix()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("-t", "--twitter_handle",
         help="Twitter handle to pull tweets against.")
     args = parser.parse_args()
-    Transform().main(args.twitter_handle)
+    Transform(args.twitter_handle).main()
